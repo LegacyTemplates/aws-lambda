1466a85fd29b995c84d94a2746b17b028d58e0ab
Darren Reid <darren@reidmail.org>
2021-01-14 15:57:05 +1100

    Test action deploy end 2 end.

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -42,7 +42,7 @@ namespace MyApp
 
     public class AppHost : AppHostBase
     {
-        public AppHost() : base("MyApp3", typeof(MyServices).Assembly) { }
+        public AppHost() : base("MyApp99", typeof(MyServices).Assembly) { }
 
         // Configure your AppHost with the necessary configuration and dependencies your App needs
         public override void Configure(Container container)
